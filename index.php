737abd40f0c37eade81e1f8fc738dac35d09f11d
Arjun Malik <67333225+arjun3011@users.noreply.github.com>
2021-04-06 13:30:40 +0530

    first php file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php  
+    function myFirstFunction($myname,$mycolor)  
+    {
+        
+        echo "<p> Hi, my name is $myname and my favorite color is $mycolor.</p>" ;
+    }
+
+myFirstFunction('arjun','red');
+myFirstFunction('jane','blue');
+
+?>
+ 
+
+ <h1><?php bloginfo('name'); ?></h1>
+ <!-- <p><?php  bloginfo('description') ;?></p> -->
