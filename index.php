1a839a92578dae1581f163af48fc50b343b0aa95
Arjun Malik <67333225+arjun3011@users.noreply.github.com>
2021-04-06 18:47:29 +0530

    index updated

--- index.php
+++ index.php
@@ -1,43 +1,14 @@
-<?php  
-    function myFirstFunction($myname,$mycolor)  
-    {
-        
-        echo "<p> Hi, my name is $myname and my favorite color is $mycolor.</p>" ;
+<?php 
+get_header();
+    while (have_posts()){
+        the_post();
+        ?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> 
+        <?php the_content();?>
+        <hr>
+
+        <?php
     }
-
-myFirstFunction('arjun','red');
-myFirstFunction('jane','blue');
-
-?>
- 
-
- <h1><?php bloginfo('name'); ?></h1>
- <p><?php  bloginfo('description') ;?></p> 
-
-
-<?php  
-    $name = array('Arjun','Malik');
-    $count=1;
-    while($count<100){
-        echo "<li>$count</li>";
-        $count++;
-
-    }
-?>
-<p>My name is <?php echo $name[0]?></p>
-
-
-<?php  
-    $name = array('Arjun','Malik','Toto','Malik');
-    $count=0;
-    while($count<count($name))
-    {
-        $arr1 = "Hi , my name is $name[$count]" ;  
-        $count++;
-        $arr2= " and my last name is $name[$count]";
-        $count++;
-        $arr=array($arr1,$arr2);
-        echo join(" ",$arr)."<br>";
-    }
+get_footer();
 ?>
 
